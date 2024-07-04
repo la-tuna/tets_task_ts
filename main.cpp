582f65ebb9d520c86456ec5ca94a0d74f5e72c68
la-tuna <65861234+la-tuna@users.noreply.github.com>
2024-07-04 22:07:26 +0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,15 @@
+﻿#include "UserInteraction.h"
+#include "Renderer.h"
+#include <vector>
+
+int main(int argc, char** argv) {
+    system("chcp 1251");
+
+    std::vector<Shape*> shapes;
+    UserInteraction::getUserInput(shapes);
+
+    Renderer renderer(shapes);
+    renderer.run(argc, argv);
+
+    return 0;
+}
